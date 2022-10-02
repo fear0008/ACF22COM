afc4e62682ccf6dc562e60d50f0fc4f68c3ed7f8
JoshuaFearnall <JoshFearnall@gmail.com>
2022-10-02 19:08:06 -0400

    Initial A12

---
+++ A12/Garter/Compilers.h
@@ -0,0 +1,100 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Fall, 2022
+* Author: Svillen Ranev - Paulo Sousa
+* Professors: Paulo Sousa
+************************************************************
+ _________________________________
+|                                 |
+| ........ GARTER LANGUAGE ...... |
+|     __    __    __    __        |
+|    /  \  /  \  /  \  /  \       |
+| __/  __\/  __\/  __\/  __\__    |
+| _/  /__/  /__/  /__/  /_____|   |
+|  \_/ \   / \   / \   / \  \___  |
+|       \_/   \_/   \_/   \___o_> |
+|                                 |
+| .. ALGONQUIN COLLEGE - 2022F .. |
+|_________________________________|
+
+*/
+
+/*
+************************************************************
+* File name: compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A12, A22, A32.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+ 
+/* Logical constants - adapt for your language */
+#define GAR_TRUE  1
+#define GAR_FALSE 0
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+/* Define your typedefs */
+typedef char			gar_char;
+typedef int				gar_intg;
+typedef float			gar_real;
+typedef void			gar_void;
+
+typedef unsigned char	gar_boln;
+typedef unsigned char	gar_byte;
+
+typedef long			gar_long;
+typedef double			gar_doub;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Reader - invokes MainReader code
+2: Scanner - invokes MainScanner code
+3: Parser - invokes MainParser code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_READER	= 'R',
+	PGM_SCANNER = 'S',
+	PGM_PARSER	= 'P'
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+gar_intg mainReader  (gar_intg argc, gar_char** argv);
+/*
+boa_intg mainScanner (boa_intg argc, boa_char** argv);
+boa_intg mainParser  (boa_intg argc, boa_char** argv);
+*/
+gar_void printLogo();
+
+#endif
